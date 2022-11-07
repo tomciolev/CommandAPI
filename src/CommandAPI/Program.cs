2aadfe072eee83e4092c2c6a12534168049980f1
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-11-07 22:46:14 +0100

    Change settings, connectionString, add secret variables

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,14 +1,21 @@
 using CommandAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+var connectionBuilder = new NpgsqlConnectionStringBuilder();
+connectionBuilder.ConnectionString = builder.Configuration["ConnectionStrings:PostgreSqlConnection"];
+connectionBuilder.Username = builder.Configuration["UserID"];
+connectionBuilder.Password = builder.Configuration["Password"];
 builder.Services.AddDbContext<CommandContext>(opts =>
 {
-    opts.UseNpgsql(builder.Configuration["ConnectionStrings:PostgreSqlConnection"]);
+    opts.UseNpgsql(connectionBuilder.ConnectionString);
 });
+
 builder.Services.AddScoped<ICommandRepository, EFCommandRepository>();
 var app = builder.Build();
 
