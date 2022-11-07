a03fb391bc9166ea20eb7fb94c639f5030992760
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-11-07 18:56:37 +0100

    Add model

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace CommandAPI.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommmandLine { get; set; }
+    }
+}
